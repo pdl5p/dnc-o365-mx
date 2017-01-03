159162721f36acc2bd590666299bd9636bea90bb
pdl5p <paul.lucas@fivep.com.au>
2017-01-03 14:29:03 +1100

    Config setup

---
+++ AppSettings.cs
@@ -0,0 +1,12 @@
+
+namespace ConsoleApplication
+{
+    public class AppSettings
+    {
+        public string CertificateThumbPrint { get; set; }
+        public string ClientId { get; set; }
+        public string TenantId { get; set; }
+        public string CertificatePassword { get; set; }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.UserSecrets;
+
+[assembly: UserSecretsId("dnc-o365-mx-0000")]
 
 namespace ConsoleApplication
 {
+
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json")
+                    .AddEnvironmentVariables()
+                    .AddUserSecrets();
+
+            var config = builder.Build();
+            var settings = new AppSettings();
+            config.Bind(settings);
+
+
+
         }
     }
 }
